c1a5e6a3c4b1efb850c95f186d6bddb9ed8f8918
Michael Bessolov <michael@oroinc.com>
2020-04-25 22:58:16 -0700

    BAP-19820: Fix unit and functional tests to upgrade PHPUnit - refactored the tests that assessed subject's internal state with any of the following methods deprecated by PHPUnit: -- assertAttributeContains -- assertAttributeContainsOnly -- assertAttributeCount -- assertAttributeEmpty -- assertAttributeGreaterThan -- assertAttributeGreaterThanOrEqual -- assertAttributeInstanceOf -- assertAttributeInternalType -- assertAttributeLessThan -- assertAttributeLessThanOrEqual -- assertAttributeNotContains -- assertAttributeNotContainsOnly -- assertAttributeNotCount -- assertAttributeNotEmpty -- assertAttributeNotEquals -- assertAttributeNotInstanceOf -- assertAttributeNotInternalType -- assertAttributeNotSame -- assertAttributeSame -- assertNotInternalType -- getObjectAttribute -- readAttribute

--- Tests/Unit/Integration/AuthorizeNetTransportTest.php
+++ Tests/Unit/Integration/AuthorizeNetTransportTest.php
@@ -5,6 +5,7 @@ namespace Oro\Bundle\AuthorizeNetBundle\Tests\Unit\Integration;
 use Oro\Bundle\AuthorizeNetBundle\Entity\AuthorizeNetSettings;
 use Oro\Bundle\AuthorizeNetBundle\Form\Type\AuthorizeNetSettingsType;
 use Oro\Bundle\AuthorizeNetBundle\Integration\AuthorizeNetTransport;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class AuthorizeNetTransportTest extends \PHPUnit\Framework\TestCase
 {
@@ -13,28 +14,33 @@ class AuthorizeNetTransportTest extends \PHPUnit\Framework\TestCase
 
     protected function setUp(): void
     {
-        $this->transport = new AuthorizeNetTransport();
+        $this->transport = new class() extends AuthorizeNetTransport {
+            public function xgetSettings(): ParameterBag
+            {
+                return $this->settings;
+            }
+        };
     }
 
     public function testInitCompiles()
     {
         $settings = new AuthorizeNetSettings();
         $this->transport->init($settings);
-        $this->assertAttributeSame($settings->getSettingsBag(), 'settings', $this->transport);
+        static::assertSame($settings->getSettingsBag(), $this->transport->xgetSettings());
     }
 
     public function testGetSettingsFormType()
     {
-        $this->assertSame(AuthorizeNetSettingsType::class, $this->transport->getSettingsFormType());
+        static::assertSame(AuthorizeNetSettingsType::class, $this->transport->getSettingsFormType());
     }
 
     public function testGetSettingsEntityFQCN()
     {
-        $this->assertSame(AuthorizeNetSettings::class, $this->transport->getSettingsEntityFQCN());
+        static::assertSame(AuthorizeNetSettings::class, $this->transport->getSettingsEntityFQCN());
     }
 
     public function testGetLabelReturnsString()
     {
-        $this->assertTrue(is_string($this->transport->getLabel()));
+        static::assertTrue(is_string($this->transport->getLabel()));
     }
 }
