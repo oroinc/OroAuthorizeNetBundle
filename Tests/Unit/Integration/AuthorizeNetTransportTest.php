b4aedcfaa2eec1eb8bb7282a0ae1abe9825f0ba9
Vova Soroka <vsoroka@oroinc.com>
2023-03-19 23:26:41 +0200

    Refactor unit tests in MailChimpBundle (#35101)

--- Tests/Unit/Integration/AuthorizeNetTransportTest.php
+++ Tests/Unit/Integration/AuthorizeNetTransportTest.php
@@ -5,42 +5,41 @@ namespace Oro\Bundle\AuthorizeNetBundle\Tests\Unit\Integration;
 use Oro\Bundle\AuthorizeNetBundle\Entity\AuthorizeNetSettings;
 use Oro\Bundle\AuthorizeNetBundle\Form\Type\AuthorizeNetSettingsType;
 use Oro\Bundle\AuthorizeNetBundle\Integration\AuthorizeNetTransport;
-use Symfony\Component\HttpFoundation\ParameterBag;
+use Oro\Component\Testing\ReflectionUtil;
 
 class AuthorizeNetTransportTest extends \PHPUnit\Framework\TestCase
 {
-    /** @var AuthorizeNetTransport */
-    private $transport;
+    private AuthorizeNetTransport $transport;
 
     protected function setUp(): void
     {
-        $this->transport = new class() extends AuthorizeNetTransport {
-            public function xgetSettings(): ParameterBag
-            {
-                return $this->settings;
-            }
-        };
+        $this->transport = new AuthorizeNetTransport();
     }
 
     public function testInitCompiles()
     {
         $settings = new AuthorizeNetSettings();
+
         $this->transport->init($settings);
-        static::assertSame($settings->getSettingsBag(), $this->transport->xgetSettings());
+
+        self::assertSame(
+            $settings->getSettingsBag(),
+            ReflectionUtil::getPropertyValue($this->transport, 'settings')
+        );
     }
 
     public function testGetSettingsFormType()
     {
-        static::assertSame(AuthorizeNetSettingsType::class, $this->transport->getSettingsFormType());
+        self::assertSame(AuthorizeNetSettingsType::class, $this->transport->getSettingsFormType());
     }
 
     public function testGetSettingsEntityFQCN()
     {
-        static::assertSame(AuthorizeNetSettings::class, $this->transport->getSettingsEntityFQCN());
+        self::assertSame(AuthorizeNetSettings::class, $this->transport->getSettingsEntityFQCN());
     }
 
     public function testGetLabelReturnsString()
     {
-        static::assertTrue(is_string($this->transport->getLabel()));
+        self::assertIsString($this->transport->getLabel());
     }
 }
