0cb7c5d1a879dbc39aa0a522de14ac5be7e8bc14
Oleksandr Protsiuk <44535554+OleksandrProtsiuk@users.noreply.github.com>
2025-07-25 17:48:44 +0300

    BAP-23106: Get rid of Symfony 7 deprecations. Part 1 (#42062)
    - Updated namespaces to use routing attributes
    - Updated routing configs to fix annotations services deprication notices
    - Disabled annotations cache
    - Updated app`s configs to use attributes for validations to fix annotations services deprecation notices
    - Updated console commands to fix "$defaultName" and "$defaultDescription deprecation notices
    - Replaced calls to the deprecated getDefaultName method with command names
    - Replaces usage of deprecated constraints with recommended
    - Removed deprecated method setDoctrineAnnotationReader calls
    - Upgrade toolkit dependencies updated
    - Updated rector rules/rulesets
    - Added new YmlFixer rules

--- Controller/SettingsController.php
+++ Controller/SettingsController.php
@@ -9,7 +9,7 @@ use Oro\Bundle\SecurityBundle\Attribute\CsrfProtection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
