e6978d6ee644a5a3158c67840b5685d12c8cdcdb
Yevhenii Zhadan <146938608+yevhenii101991@users.noreply.github.com>
2025-10-28 12:31:17 +0200

    BB-17384: Payment integrations from the first non global organization are available in the second non-global organization (#43007)

--- Tests/Functional/DataFixtures/LoadAuthorizeNetChannelData.php
+++ Tests/Functional/DataFixtures/LoadAuthorizeNetChannelData.php
@@ -3,17 +3,32 @@
 namespace Oro\Bundle\AuthorizeNetBundle\Tests\Functional\DataFixtures;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\AuthorizeNetBundle\Entity\AuthorizeNetSettings;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOtherOrganizations;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadUser;
 use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class LoadAuthorizeNetChannelData extends AbstractFixture implements ContainerAwareInterface
+class LoadAuthorizeNetChannelData extends AbstractFixture implements DependentFixtureInterface, ContainerAwareInterface
 {
     use ContainerAwareTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDependencies(): array
+    {
+        return [
+            LoadOrganization::class,
+            LoadUser::class,
+            LoadOtherOrganizations::class
+        ];
+    }
 
     private array $channelData = [
         [
@@ -21,18 +36,28 @@ class LoadAuthorizeNetChannelData extends AbstractFixture implements ContainerAw
             'type' => 'authorize_net',
             'enabled' => true,
             'reference' => 'authorize_net:channel_1',
+            'organization' => LoadOrganization::ORGANIZATION,
         ],
         [
             'name' => 'AuthorizeNet2',
             'type' => 'authorize_net',
             'enabled' => true,
             'reference' => 'authorize_net:channel_2',
+            'organization' => LoadOrganization::ORGANIZATION,
         ],
         [
             'name' => 'AuthorizeNet3',
             'type' => 'authorize_net',
             'enabled' => false,
             'reference' => 'authorize_net:channel_3',
+            'organization' => LoadOrganization::ORGANIZATION,
+        ],
+        [
+            'name' => 'AuthorizeNet4',
+            'type' => 'authorize_net',
+            'enabled' => true,
+            'reference' => 'authorize_net:channel_4',
+            'organization' => LoadOtherOrganizations::ORGANIZATION_1
         ],
     ];
 
@@ -43,7 +68,6 @@ class LoadAuthorizeNetChannelData extends AbstractFixture implements ContainerAw
     {
         $userManager = $this->container->get('oro_user.manager');
         $admin = $userManager->findUserByEmail(LoadAdminUserData::DEFAULT_ADMIN_EMAIL);
-        $organization = $manager->getRepository(Organization::class)->getFirst();
 
         foreach ($this->channelData as $data) {
             $entity = new Channel();
@@ -51,7 +75,7 @@ class LoadAuthorizeNetChannelData extends AbstractFixture implements ContainerAw
             $entity->setType($data['type']);
             $entity->setEnabled($data['enabled']);
             $entity->setDefaultUserOwner($admin);
-            $entity->setOrganization($organization);
+            $entity->setOrganization($this->getReference($data['organization']));
             $entity->setTransport(new AuthorizeNetSettings());
             $this->setReference($data['reference'], $entity);
 
--- Tests/Functional/Entity/Repository/AuthorizeNetSettingsRepositoryTest.php
+++ Tests/Functional/Entity/Repository/AuthorizeNetSettingsRepositoryTest.php
@@ -5,7 +5,9 @@ namespace Oro\Bundle\AuthorizeNetBundle\Tests\Functional\Entity\Repository;
 use Oro\Bundle\AuthorizeNetBundle\Entity\AuthorizeNetSettings;
 use Oro\Bundle\AuthorizeNetBundle\Entity\Repository\AuthorizeNetSettingsRepository;
 use Oro\Bundle\AuthorizeNetBundle\Tests\Functional\DataFixtures\LoadAuthorizeNetChannelData;
+use Oro\Bundle\SecurityBundle\Authentication\Token\UsernamePasswordOrganizationToken;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\UserBundle\Migrations\Data\ORM\LoadAdminUserData;
 
 class AuthorizeNetSettingsRepositoryTest extends WebTestCase
 {
@@ -22,6 +24,19 @@ class AuthorizeNetSettingsRepositoryTest extends WebTestCase
 
     public function testGetEnabledSettingsByType()
     {
+        $userManager = self::getContainer()->get('oro_user.manager');
+        $admin = $userManager->findUserByEmail(LoadAdminUserData::DEFAULT_ADMIN_EMAIL);
+
+        $token = new UsernamePasswordOrganizationToken(
+            $admin,
+            'admin',
+            'main',
+            $admin->getOrganization(),
+            $admin->getRoles()
+        );
+
+        $this->getContainer()->get('security.token_storage')->setToken($token);
+
         $enabledSettings = $this->repository->getEnabledSettingsByType('authorize_net');
         self::assertCount(2, $enabledSettings);
     }
