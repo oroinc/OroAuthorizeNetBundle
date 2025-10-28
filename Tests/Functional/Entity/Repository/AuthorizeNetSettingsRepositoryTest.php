f7eb2b7b708ae8f79b140eab61b59f43dc70a74a
Yevhenii Zhadan <146938608+yevhenii101991@users.noreply.github.com>
2025-10-28 12:31:03 +0200

    BB-17384: Payment integrations from the first non global organization… (#42952)

--- Tests/Functional/Entity/Repository/AuthorizeNetSettingsRepositoryTest.php
+++ Tests/Functional/Entity/Repository/AuthorizeNetSettingsRepositoryTest.php
@@ -5,7 +5,10 @@ namespace Oro\Bundle\AuthorizeNetBundle\Tests\Functional\Entity\Repository;
 use Oro\Bundle\AuthorizeNetBundle\Entity\AuthorizeNetSettings;
 use Oro\Bundle\AuthorizeNetBundle\Entity\Repository\AuthorizeNetSettingsRepository;
 use Oro\Bundle\AuthorizeNetBundle\Tests\Functional\DataFixtures\LoadAuthorizeNetChannelData;
+use Oro\Bundle\SecurityBundle\Authentication\Token\UsernamePasswordOrganizationToken;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadOrganization;
+use Oro\Bundle\TestFrameworkBundle\Tests\Functional\DataFixtures\LoadUser;
 
 class AuthorizeNetSettingsRepositoryTest extends WebTestCase
 {
@@ -23,6 +26,15 @@ class AuthorizeNetSettingsRepositoryTest extends WebTestCase
 
     public function testGetEnabledSettingsByType()
     {
+        $organization = $this->getReference(LoadOrganization::ORGANIZATION);
+        $adminToken = new UsernamePasswordOrganizationToken(
+            $this->getReference(LoadUser::USER),
+            'key',
+            $organization
+        );
+
+        $this->getContainer()->get('security.token_storage')->setToken($adminToken);
+
         $enabledSettings = $this->repository->getEnabledSettingsByType('authorize_net');
         self::assertCount(2, $enabledSettings);
     }
